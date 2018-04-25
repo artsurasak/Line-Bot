1cdb4b24181af5676936a3f60a7e34d1a047b72b
Surasak <artvkm@gmail.com>
2018-04-25 17:05:26 +0700

    commit

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-var express = require('express')
+var express = require('express');
+var middleware = require('@line/bot-sdk').middleware
+var JSONParseError = require('@line/bot-sdk').JSONParseError
+var SignatureValidationFailed = require('@line/bot-sdk').SignatureValidationFailed
 var bodyParser = require('body-parser')
 var app = express()
-var http = require('http');
-var server = http.createServer(app);
-var cors = require('cors')
+var http = require('http')
+var server = http.createServer(app)
 
-app.use(cors())
-app.use(bodyParser.json())
+const config = {
+  channelAccessToken: 'wNGFPyRYMYL1ZuaxZBZN+gw/1FOMR52WrEGtybbWfLlXsyIJU+2NUrUB70DQzT1ID4Jhwh34P4mc3fFdIEI7rtjUToiUzOlxjmtEfS/mekbd01VYgAYZybnHi9y0Q3REK0oaVFJricEwTEehEYaOGgdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '553bc1a177fb9b042fa30b3544357169'
+}
 
-app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(middleware(config))
 
-
-app.get('/', function (req, res) {
-  res.send('Hello')
+app.post('/webhook', (req, res) => {
+  res.json(req.body.events) // req.body will be webhook event object
 })
+
+app.use((err, req, res, next) => {
+  if (err instanceof SignatureValidationFailed) {
+    res.status(401).send(err.signature)
+    return
+  } else if (err instanceof JSONParseError) {
+    res.status(400).send(err.raw)
+    return
+  }
+  next(err) // will throw default 500
+})
+// app.get('/', function (req, res) {
+//   res.send('Hello')
+// });
 
 /*
 app.listen(app.get('port'), function () {
